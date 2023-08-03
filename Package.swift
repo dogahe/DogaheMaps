ad299a9eb08e078ffe022620fc590e67158982ec
dogahe <dogahe@google.com>
2023-08-03 13:34:17 -0400

    save

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
   ], dependencies: [],
   targets: [
     .binaryTarget(
-      name: "GoogleMaps", url: "https://dl.google.com/geosdk/swiftpm/0.0.0/GoogleMaps.zip",
+      name: "GoogleMaps", url: "https://github.com/dogahe/DogaheMaps/releases/download/1.0.0/GoogleMaps_3p.xcframework.zip",
       checksum: "c46aa291aee045df611c1744ec28e20b55dab2e8d365bc1315d53296ac1a9ad9"
     ),
     .target(
@@ -23,7 +23,7 @@ let package = Package(
       publicHeadersPath: "Sources"
     ),
     .binaryTarget(
-      name: "GoogleMapsCore", url: "https://dl.google.com/geosdk/swiftpm/0.0.0/GoogleMapsCore.zip",
+      name: "GoogleMapsCore", url: "https://github.com/dogahe/DogaheMaps/releases/download/1.0.0/GoogleMapsCore_3p.xcframework.zip",
       checksum: "5c947fae109175ea5ff2e2c5ada6bd6b571f3730f7aa4d844b7abda7a5612bdb"
     ),
     .target(
@@ -34,7 +34,7 @@ let package = Package(
       publicHeadersPath: "Sources"
     ),
     .binaryTarget(
-      name: "GoogleMapsBase", url: "https://dl.google.com/geosdk/swiftpm/0.0.0/GoogleMapsBase.zip",
+      name: "GoogleMapsBase", url: "https://github.com/dogahe/DogaheMaps/releases/download/1.0.0/GoogleMapsBase_3p.xcframework.zip",
       checksum: "75b876c1444d7dcfc17cb6efa1e9fc6773d9bf2f154d95d76d263fde0ac198d0"
     ),
     .target(
