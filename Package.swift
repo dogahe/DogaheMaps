ad34ab5d9811576a084c00f301fa4649b486b084
Behzad Dogahe <dogahe@google.com>
2023-09-21 11:36:40 -0400

    latest

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
   ], dependencies: [],
   targets: [
     .binaryTarget(
-      name: "GoogleMaps", url: "https://github.com/dogahe/DogaheMaps/releases/download/1.0.0/GoogleMaps_3p.xcframework.zip",
-      checksum: "c46aa291aee045df611c1744ec28e20b55dab2e8d365bc1315d53296ac1a9ad9"
+      name: "GoogleMaps", url: "https://github.com/dogahe/DogaheMaps/releases/download/1.0.10/GoogleMaps_3p.xcframework.zip",
+      checksum: "ee9f35cb5059dacd89c2f1c85bd1a6326f8eb7dc885fc8b3ccfcbf87f48a427d"
     ),
     .target(
       name: "GoogleMapsTarget",
@@ -23,8 +23,8 @@ let package = Package(
       publicHeadersPath: "Sources"
     ),
     .binaryTarget(
-      name: "GoogleMapsCore", url: "https://github.com/dogahe/DogaheMaps/releases/download/1.0.0/GoogleMapsCore_3p.xcframework.zip",
-      checksum: "5c947fae109175ea5ff2e2c5ada6bd6b571f3730f7aa4d844b7abda7a5612bdb"
+      name: "GoogleMapsCore", url: "https://github.com/dogahe/DogaheMaps/releases/download/1.0.10/GoogleMapsCore_3p.xcframework.zip",
+      checksum: "2fc61c33415bf354dd80307abbb707553682cb37b3773874de11797a0a2c1344"
     ),
     .target(
       name: "GoogleMapsCoreTarget",
@@ -34,8 +34,8 @@ let package = Package(
       publicHeadersPath: "Sources"
     ),
     .binaryTarget(
-      name: "GoogleMapsBase", url: "https://github.com/dogahe/DogaheMaps/releases/download/1.0.0/GoogleMapsBase_3p.xcframework.zip",
-      checksum: "75b876c1444d7dcfc17cb6efa1e9fc6773d9bf2f154d95d76d263fde0ac198d0"
+      name: "GoogleMapsBase", url: "https://github.com/dogahe/DogaheMaps/releases/download/1.0.10/GoogleMapsBase_3p.xcframework.zip",
+      checksum: "78e3dc136c8b040b8b51195333c1fe73787b909559aacc9da5258938b3a90b52"
     ),
     .target(
       name: "GoogleMapsBaseTarget",
@@ -46,3 +46,7 @@ let package = Package(
     ),
   ]
 )
+
+// checksum: "c46aa291aee045df611c1744ec28e20b55dab2e8d365bc1315d53296ac1a9ad9"
+// checksum: "5c947fae109175ea5ff2e2c5ada6bd6b571f3730f7aa4d844b7abda7a5612bdb"
+// checksum: "75b876c1444d7dcfc17cb6efa1e9fc6773d9bf2f154d95d76d263fde0ac198d0"
