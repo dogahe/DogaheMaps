9041d555160bb1610d04dea910f93fd39b722e31
Behzad Dogahe <dogahe@google.com>
2023-11-15 11:43:37 -0500

    added lorry links and updated resources

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
   ], dependencies: [],
   targets: [
     .binaryTarget(
-      name: "GoogleMaps", url: "https://github.com/dogahe/DogaheMaps/releases/download/1.0.10/GoogleMaps_3p.xcframework.zip",
-      checksum: "ee9f35cb5059dacd89c2f1c85bd1a6326f8eb7dc885fc8b3ccfcbf87f48a427d"
+      name: "GoogleMaps", url: "https://dl.google.com/geosdk/swiftpm/8.3.0/GoogleMaps_3p.xcframework.zip",
+      checksum: "4d9b8a4836d52eb7ef6257968853a9949b02cde299a05fe7389efab54c59128c"
     ),
     .target(
       name: "GoogleMapsTarget",
@@ -42,8 +42,8 @@ let package = Package(
       ]
     ),
     .binaryTarget(
-      name: "GoogleMapsCore", url: "https://github.com/dogahe/DogaheMaps/releases/download/1.0.10/GoogleMapsCore_3p.xcframework.zip",
-      checksum: "2fc61c33415bf354dd80307abbb707553682cb37b3773874de11797a0a2c1344"
+      name: "GoogleMapsCore", url: "https://dl.google.com/geosdk/swiftpm/8.3.0/GoogleMapsCore_3p.xcframework.zip",
+      checksum: "833bdd8b455bddb54f02330ea8339a47003403293392ad37059e7cd6e59c283c"
     ),
     .target(
       name: "GoogleMapsCoreTarget",
@@ -53,8 +53,8 @@ let package = Package(
       publicHeadersPath: "Sources"
     ),
     .binaryTarget(
-      name: "GoogleMapsBase", url: "https://github.com/dogahe/DogaheMaps/releases/download/1.0.10/GoogleMapsBase_3p.xcframework.zip",
-      checksum: "78e3dc136c8b040b8b51195333c1fe73787b909559aacc9da5258938b3a90b52"
+      name: "GoogleMapsBase", url: "https://dl.google.com/geosdk/swiftpm/8.3.0/GoogleMapsBase_3p.xcframework.zip",
+      checksum: "5d0b7a00385bfaddbe9acb39762e5e62644ca98f7cc6dcaaaf3737d2a0084854"
     ),
     .target(
       name: "GoogleMapsBaseTarget",
@@ -64,7 +64,7 @@ let package = Package(
       publicHeadersPath: "Sources"
     ),
     .binaryTarget(
-      name: "GoogleMapsM4B", url: "https://github.com/dogahe/DogaheMaps/releases/download/1.0.10/GoogleMapsM4B_3p.xcframework.zip",
+      name: "GoogleMapsM4B", url: "https://dl.google.com/geosdk/swiftpm/8.3.0/GoogleMapsM4B_3p.xcframework.zip",
       checksum: "26b92a9e5872f4e6375528fd4b7aa7a8c164a94a31e86e3eca51932cd52be790"
     ),
     .target(
