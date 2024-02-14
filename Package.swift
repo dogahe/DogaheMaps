e0fbba081e45187d47898b39fc6bbdf72f346e52
Behzad Dogahe <dogahe@google.com>
2024-02-14 09:10:41 -0500

    change

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ),
     .target(
       name: "GoogleMapsTarget",
-      dependencies: ["GoogleMaps"],
+      dependencies: ["GoogleMaps", "GoogleMapsCore", "GoogleMapsBase", "GoogleMapsM4B"],
       path: "Maps",
       sources: ["dummy.m"],
       resources: [.copy("Resources/GoogleMapsResources/GoogleMaps.bundle")],
