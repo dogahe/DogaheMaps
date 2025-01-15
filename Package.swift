8a0a4c981228f68e7342a97f366d119736e26e2e
Behzad Dogahe <dogahe@google.com>
2025-01-15 11:28:05 -0500

    Updated to latest from GoogleMaps

--- Package.swift
+++ Package.swift
@@ -1,4 +1,18 @@
 // swift-tools-version: 5.5
+//
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 import PackageDescription
 
@@ -8,16 +22,12 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "GoogleMaps",
-      url: "https://dl.google.com/geosdk/swiftpm/9.0.0/GoogleMaps_3p.xcframework.zip",
-      checksum: "997a67052607339e379a46d281ee4d198c6448f59176ff03e0a5da5bb315cd14"
+      url: "https://dl.google.com/geosdk/swiftpm/9.3.0/GoogleMaps_3p.xcframework.zip",
+      checksum: "13fbc15bdadd25b43d3aa14d7280456f64157f6b44392188dab5623998bdd8d6"
     ),
     .target(
       name: "GoogleMapsTarget",
-      dependencies: [
-        "GoogleMaps",
-        "GoogleMapsBase",
-        "GoogleMapsCore",
-      ],
+      dependencies: ["GoogleMaps"],
       path: "Maps",
       sources: ["GMSEmpty.m"],
       resources: [.copy("Resources/GoogleMapsResources/GoogleMaps.bundle")],
@@ -25,33 +35,23 @@ let package = Package(
       linkerSettings: [
         .linkedLibrary("c++"),
         .linkedLibrary("z"),
+        .linkedFramework("Accelerate"),
         .linkedFramework("Contacts"),
         .linkedFramework("CoreData"),
         .linkedFramework("CoreGraphics"),
+        .linkedFramework("CoreImage"),
         .linkedFramework("CoreLocation"),
         .linkedFramework("CoreTelephony"),
-        .linkedFramework("QuartzCore"),
-        .linkedFramework("SystemConfiguration"),
-        .linkedFramework("UIKit"),
-        .linkedFramework("Accelerate"),
-        .linkedFramework("CoreImage"),
         .linkedFramework("CoreText"),
         .linkedFramework("GLKit"),
         .linkedFramework("ImageIO"),
         .linkedFramework("Metal"),
         .linkedFramework("OpenGLES"),
         .linkedFramework("QuartzCore"),
+        .linkedFramework("Security"),
+        .linkedFramework("SystemConfiguration"),
+        .linkedFramework("UIKit"),
       ]
-    ),
-    .binaryTarget(
-      name: "GoogleMapsCore",
-      url: "https://dl.google.com/geosdk/swiftpm/9.0.0/GoogleMapsCore_3p.xcframework.zip",
-      checksum: "290f4002db479331d18fb9233e3015a1eb0d9abc16a765648e60590f34b0d691"
-    ),
-    .binaryTarget(
-      name: "GoogleMapsBase",
-      url: "https://dl.google.com/geosdk/swiftpm/9.0.0/GoogleMapsBase_3p.xcframework.zip",
-      checksum: "de00cb1d8557ee90a2fa7dd73060d74ddaf28d6d137082d6df147535b5eb4a01"
     ),
   ]
 )
